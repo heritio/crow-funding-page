5c92e0347653d1e204fd197a7188e419ba219527
Heritier Akilimali <herri_93@hotmal.com>
2021-07-12 23:27:44 +0200

    Added styling for modal and active states

--- script.js
+++ script.js
@@ -2,36 +2,37 @@
 /**Bookmark glow */
 let bookmarkBtn = document.querySelector(".section-1__bookmark-btn");
 let bookmarkCircle = document.querySelector(".bookmark-circle");
-bookmarkBtn.addEventListener("mouseover", ()=>{
-    if(bookmarkCircle.classList.contains("circle-light")){
-        return;
-    }
-   bookmarkCircle.classList.add("circle-light")
-      
-    bookmarkBtn.addEventListener("mouseout", ()=>{
-        bookmarkCircle.classList.remove("circle-light")
-    })
+let bookmarkPath = document.querySelector(".bookmark-path");
+bookmarkBtn.addEventListener("click", ()=>{
+   bookmarkCircle.classList.toggle("circle-bookmarked")
+   bookmarkPath.classList.toggle("path-bookmarked");
+   bookmarkBtn.classList.toggle("section-1__bookmark-btn--active");
 })
-bookmarkCircle.addEventListener("mouseover", ()=>{
-    if(bookmarkCircle.classList.contains("circle-light")){
-        return;
-    }
-    bookmarkCircle.classList.add("circle-light")
-       
-     bookmarkCircle.addEventListener("mouseout", ()=>{
-         bookmarkCircle.classList.remove("circle-light")
-     })
- })
-
  /**Status bar code */
  let backProjectBtn = document.querySelector(".section-1__back-project-btn");
  let statusBar = document.querySelector(".section-2__status-bar-inner");
- let currentStatus = statusBar.style.maxWidth;
- let totalBackers = document.querySelector(".section-2_total-backers");
+ let currentStatus = 65;
+ statusBar.style.width = currentStatus + "%";
 
- backProjectBtn.addEventListener("click", ()=> {
-    statusBar.style.maxWidth = `calc("${currentStatus + 1}px")`;
-    totalBackers.textContent = Number(totalBackers.textContent);
- })
+ let totalBackersText = document.querySelector(".section-2_total-backers");
+ let numberOfBackers = 5.007;
+ let numberOfBackersString = String(numberOfBackers);
+ totalBackersText.textContent = `${numberOfBackersString[0]},${numberOfBackersString[2]}${numberOfBackersString[3]}${numberOfBackersString[4]}`;
+
+backProjectBtn.addEventListener("click", ()=> {
+    if(currentStatus >= 100){
+        return;
+    }
+    /**Number of backers event */
+    numberOfBackers = numberOfBackers + 0.001;
+    let numberOfBackersString = String(numberOfBackers);
+    totalBackersText.textContent = `${numberOfBackersString[0]},${numberOfBackersString[2]}${numberOfBackersString[3]}${numberOfBackersString[4]}`;
+    /**Statusbar event */
+    currentStatus = currentStatus + .5;
+    statusBar.style.width = `${currentStatus}%`;
+})
+
+
+
 
 
